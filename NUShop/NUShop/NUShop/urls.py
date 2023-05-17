b12b5d4aeb9a7cf62d5bcbf9a02fe99aa1ce62d4
Tsui Yi Wern <nicoletyw505@gmail.com>
2023-05-18 00:17:52 +0800

    implemented register and login feature

--- NUShop/NUShop/NUShop/urls.py
+++ NUShop/NUShop/NUShop/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from core.views import index, contact
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
