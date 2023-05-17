50a8f56c3a68ae2afc2f4cbba55803e1d9f698f7
lay <yonglian1845@gmail.com>
2023-05-18 00:18:54 +0800

    implemented product listing and product page

--- NUShop/NUShop/NUShop/urls.py
+++ NUShop/NUShop/NUShop/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
 urlpatterns = [
     path('', index, name='index'),
+    path('products/', include('product.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
