53410708ad22553c6517cb2823de95ff02feed23
Tsui Yi Wern <nicoletyw505@gmail.com>
2023-05-19 01:25:45 +0800

    added search feature

--- NUShop/NUShop/product/urls.py
+++ NUShop/NUShop/product/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'product'
 
 urlpatterns = [
+    path('', views.products, name='products'),
     path('<int:pk>/', views.detail, name='detail'),
 ]
--- NUShop/NUShop/product/views.py
+++ NUShop/NUShop/product/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render, get_object_or_404
-from . models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+
+from . models import Category, Product
+
+def products(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    products = Product.objects.filter(is_sold=False)
+
+    if category_id:
+        products = products.filter(category_id=category_id)
+
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'product/products.html', {
+        'products': products,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    })
 
 # Create your views here.
 def detail(request, pk):
