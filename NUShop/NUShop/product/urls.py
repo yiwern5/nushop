9bd0907f0a69bbbb52209a338cac1ca1c9d60512
lay <yonglian1845@gmail.com>
2023-05-21 21:41:10 +0800

    added delete and edit functionality

--- NUShop/NUShop/product/urls.py
+++ NUShop/NUShop/product/urls.py
@@ -6,5 +6,6 @@ app_name = 'product'
 urlpatterns = [
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
-    
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
