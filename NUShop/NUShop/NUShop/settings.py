c2b7ee41a379ab78496dd5e19e6645038a39e216
Tsui Yi Wern <nicoletyw505@gmail.com>
2023-05-22 19:16:53 +0800

    added chat feature

--- NUShop/NUShop/NUShop/settings.py
+++ NUShop/NUShop/NUShop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
     'core',
     'dashboard',
     'product',
--- NUShop/NUShop/NUShop/urls.py
+++ NUShop/NUShop/NUShop/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('products/', include('product.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('chat.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
