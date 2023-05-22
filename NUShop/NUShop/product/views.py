2d50d6ab6f99cd4f1d686cf327b1b6bdaa6d40e6
Tsui Yi Wern <nicoletyw505@gmail.com>
2023-05-22 15:41:14 +0800

    added search and filter feature

--- NUShop/NUShop/product/views.py
+++ NUShop/NUShop/product/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from . models import Product
+from django.db.models import Q
+
+from . models import Category, Product
 from .forms import NewProductForm, EditProductForm
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+def products(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    products = Product.objects.filter(is_sold=False)
+
+    if category_id:
+        products = products.filter(category_id=category_id)
+
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'product/products.html', {
+        'products': products,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    })
+
 def detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     """
